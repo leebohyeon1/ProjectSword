cb238b0684570ebe8551a37f4002a9f75824e3c9
Lee Bohyeon <bohyeonyoyo@gmail.com>
2024-07-22 16:28:24 +0900

    ChainLighting
    아직 만든건 아니고 준비 중

---
+++ Assets/Scripts/Sword/SwordType/Guided/Tenkai/ChainLighting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainLighting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this, 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
